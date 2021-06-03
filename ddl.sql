b2fcef4bd19ce49b8cb2dc9a3d425ae8333bf04b
poojadhole94 <68541008+poojadhole94@users.noreply.github.com>
2021-06-03 15:35:59 +0530

    Create ddl.sql

---
+++ ddl.sql
@@ -0,0 +1,12 @@
+USE DATABASE  "AIGI_EDR_COMMONMODEL_QA";  
+USE SCHEMA "COMMON"; 
+
+
+create or replace TABLE "hub_address_test" (
+	"AddressHKey" VARCHAR(32) NOT NULL,
+	"LoadTs" TIMESTAMP_NTZ(9),
+	"RecSrcSystCd" VARCHAR(30),
+	"RecSrcBusUnitCd" VARCHAR(30),
+	"AddressId" VARCHAR(100),
+	primary key ("AddressHKey")
+);
